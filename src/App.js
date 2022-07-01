6c7c1091334953d1f4188bd8e42d4b05eb198ad0
Tony Edgal <tonyedgal@gmail.com>
2022-07-01 21:13:52 +0100

    feat: added scrolltotop feature

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
-// import { useEffect } from "react";
+import { useEffect } from "react";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
-  // useLocation,
+  useLocation,
 } from "react-router-dom";
 import { ThemeProvider } from "@mui/material";
 import React from "react";
 import Home from "./pages/Home";
 import { theme } from "./styles/theme";
 
-// const ScrollToTop = ({ children }) => {
-//   const location = useLocation();
+const ScrollToTop = ({ children }) => {
+  const location = useLocation();
 
-//   useEffect(() => {
-//     window.scrollTo(0, 0);
-//   }, [location]);
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
 
-//   return <>{children}</>;
-// };
+  return <>{children}</>;
+};
 
 const App = () => {
   return (
     <>
       <ThemeProvider theme={theme}>
         <Router>
-          {/* <ScrollToTop>
-          </ScrollToTop> */}
-          <Routes>
-            <Route path="/" element={<Home />} />
-          </Routes>
+          <ScrollToTop>
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </ScrollToTop>
         </Router>
       </ThemeProvider>
     </>
